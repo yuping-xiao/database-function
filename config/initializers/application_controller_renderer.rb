2d1336786c2a623baebd79fc387d2e7e7af6b66a
yuping-xiao <yupingxiao94@gmail.com>
2020-05-07 01:04:03 +0900

    added image-submit-function

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e35dcc0f45f769c7cc4222a3801fd37e15a2d8c1e8b81e5dabdbe177ebd0affb3546c2bae75a95f09768491d868cd8f65a437d8c7718a4fc0d62cbbf3b8d703d'
